ee55ee6b4c6cfc6e33a17eb19f2dca7e17f2fdb4
Tyler Austin <tyler@webguyty.com>
2020-07-22 20:40:13 -0700

    big update to layout of all sections

--- webpack.common.js
+++ webpack.common.js
@@ -12,26 +12,26 @@ module.exports = {
         use: ['html-loader'],
       },
       {
-        test: /\.(svg|png|jpg|gif|pdf)$/,
+        test: /\.(svg|png|jpg|gif)$/,
         use: {
           loader: 'file-loader',
           options: {
-            name: '[name].[ext]',
-            // name: '[name].[hash].[ext]',
+            // name: '[name].[ext]',
+            name: '[name].[hash].[ext]',
             outputPath: 'imgs',
           },
         },
       },
-      // {
-      //   test: /\.pdf$/,
-      //   use: {
-      //     loader: 'file-loader',
-      //     options: {
-      //       name: '[name].[ext]',
-      //       outputPath: 'imgs',
-      //     },
-      //   },
-      // },
+      {
+        test: /\.pdf$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'resume',
+          },
+        },
+      },
     ],
   },
 };
