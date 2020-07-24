a4f4cf4c1f6f5c5c8138e683b611df4eed51ee14
Tyler Austin <tyler@webguyty.com>
2020-07-23 20:13:16 -0700

    mobile updating

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import './main.scss';
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+import './main.scss';
 
 import './scripts/slider';
 import './scripts/scrollSpy';
