7c6cd019d1bc260d23b7e21529f7427697db93c3
Tyler <tyler@webguyty.com>
2021-01-21 22:47:04 -0800

    form errors fixed, works flawlessly

--- src/scripts/sendEmail.js
+++ src/scripts/sendEmail.js
@@ -4,13 +4,12 @@ emailSubmit.addEventListener("submit", sendEmail);
 
 function sendEmail(e) {
   e.preventDefault();
-  const name = document.getElementById("form_name").value;
-  const email = document.getElementById("form_email").value;
-  const phone = document.getElementById("form_phone").value;
-  const message = document.getElementById("form_message").value;
-
-  const success = document.getElementById("form_success");
-  const fail = document.getElementById("form_fail");
+  const name = document.getElementById("form_name"),
+    email = document.getElementById("form_email"),
+    phone = document.getElementById("form_phone"),
+    message = document.getElementById("form_message"),
+    success = document.getElementById("form_success"),
+    fail = document.getElementById("form_fail");
 
   console.log(name, email, phone, message);
 
@@ -19,9 +18,33 @@ function sendEmail(e) {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ name, email, phone, message }),
+    body: JSON.stringify({
+      name: name.value,
+      email: email.value,
+      phone: phone.value,
+      message: message.value,
+    }),
   })
-    .then((res) => res.json())
-    .then((data) => console.log("Success", data))
-    .catch((err) => console.log("error", err));
+    .then((res) => {
+      if (!res.ok) {
+        throw Error(res.statusText);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      success.style.display = "block";
+      name.value = "";
+      email.value = "";
+      phone.value = "";
+      message.value = "";
+    })
+    .catch((error) => {
+      fail.style.display = "block";
+      console.log(error);
+    });
+
+  setTimeout(() => {
+    success.style.display = "none";
+    fail.style.display = "none";
+  }, 5000);
 }
